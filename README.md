66057e01b7fbb254398f19dc3e03ec592d385d26
Michel Kohanim <michel@universal-devices.com>
2024-11-10 18:07:52 -0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
-# oadr30
-
-OpenADR 3.0 VEN Implementation
+# Open Source OpenADR 3.0 VEN Implementation 
 
 Use pip to install:
 
 ```bash
-pip install oadr30
+    pip install oadr30_ven_ud
+```
+
+Review two test files:
+tests/events_test.py - You can use this to pass OpenADR cert testing
+test/price_server_test.py - You can use this to populate the VTN with prices from Olivine price servers. 
 
 
 # oadr3 YAML
--- setup.py
+++ setup.py
@@ -1,25 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='oadr30',
+    name='oadr30_ven_ud',
     version='1.0.0',
     packages=find_packages(),
-    description='OpenADR 3.0 Reference VEN Implementation',
+    description='Open Source OpenADR 3.0 VEN Implementation',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='Michel Kohanim',
     author_email='support@universal-devices.com',
     url='https://github.com/universaldevices/oadr30.git',
     install_requires=[
-#	'jsonschema>=4.17.3',
-#	'pytz>=2023.3',
 	'requests>=2.25.1',
-#	'schedule>=1.2.0'
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.9',
 )
